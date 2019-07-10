7a3dea32fb1829dd039e3cc7d1629c78a608ab2b
HasanMu <obitorin.or@gmail.com>
2019-07-10 08:13:05 +0700

    add cors

--- routes/api.php
+++ routes/api.php
@@ -17,8 +17,10 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::resource('/siswa', 'SiswaController');
-Route::resource('/guru', 'Api\GuruController');
-Route::resource('/category', 'Api\CategoryController');
-Route::resource('/tag', 'Api\TagController');
-Route::resource('/artikel', 'Api\ArtikelController');
+Route::group(['middleware' => ['cors']], function () {
+    Route::resource('/siswa', 'SiswaController');
+    Route::resource('/guru', 'Api\GuruController');
+    Route::resource('/category', 'Api\CategoryController');
+    Route::resource('/tag', 'Api\TagController');
+    Route::resource('/artikel', 'Api\ArtikelController');
+});
